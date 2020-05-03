521b514d170dcc23b4bf1b03b883fa98d336a28e
Jean-Baptiste Nizet <jb@ninja-squad.com>
2020-05-03 20:38:51 +0200

    remove generated app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var _ = require('lodash');
-var users = [
-    { 'user': 'barney', 'active': false },
-    { 'user': 'fred', 'active': false },
-    { 'user': 'pebbles', 'active': true }
-];
-var result = _.findIndex(users, function (o) { return o.user == 'fred'; });
-console.log(result);
